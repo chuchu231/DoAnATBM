e37d477eb2bea21175b90707c91b5d9c853fd2e4
88Owner <116443373+88Owner@users.noreply.github.com>
2024-04-09 18:43:15 +0700

    Add Func.sql

---
+++ Create_User/GetRolePrivileges.sql
@@ -0,0 +1,11 @@
+﻿CREATE OR REPLACE PROCEDURE GetRolePrivileges(Role_name IN VARCHAR2, ResultCursor OUT SYS_REFCURSOR)
+AS
+    upper_role_name VARCHAR2(100);
+BEGIN
+    upper_role_name := UPPER(Role_name);
+    
+    OPEN ResultCursor FOR
+    SELECT p.role, p.table_name, p.privilege
+    FROM ROLE_TAB_PRIVS p
+    WHERE p.role = upper_role_name;
+END;
